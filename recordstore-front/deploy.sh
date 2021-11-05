94b9cab955776e6492645f9919dc4d1da0b2ce8a
Anabelle <you@example.com>
2021-11-05 11:11:08 -0400

    edits

--- recordstore-front/deploy.sh
+++ recordstore-front/deploy.sh
@@ -1,1 +1,19 @@
-#!/usr/bin/env sh# abort on errorsset -e# buildyarn run build# navigate into the build output directorycd dist# if you are deploying to a custom domain# echo 'www.example.com' > CNAMEgit initgit add -Agit commit -m 'deploy'git push -f git@github.com:anabellephung/record-store.git master:gh-pagescd -
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:anabellephung/record-store.git master:gh-pages
+
+cd -
--- recordstore-front/vue.config.js
+++ recordstore-front/vue.config.js
@@ -1,1 +1,5 @@
-module.exports = {    publicPath: process.env.NODE_ENV === 'production'    ? '/record-store/'    : '/'}
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/record-store/'
+    : '/'
+}
